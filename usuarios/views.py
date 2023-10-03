4c5b5bfa34f0333996cc1aeff23803914c2638e4
vinicius-oliveiracr <vinicius.cr.o.3@gmail.com>
2023-10-03 01:44:34 -0300

    first commit

---
+++ usuarios/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+
+# Create your views here.
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        primeiro_nome = request.POST.get('primeiro_nome')
+        ultimo_nome = request.POST.get('ultimo_nome')
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        email = request.POST.get('email')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'As senhas não coincidem.')
+            return redirect('/usuarios/cadastro/')
+        if len(senha) < 8:
+            messages.add_message(request, constants.ERROR, 'Sua senha deve ter no mínimo 8 caracteres.')
+            return redirect('/usuarios/cadastro/')
+        
+        try:
+            user = User.objects.create_user(
+                first_name = primeiro_nome,
+                last_name = ultimo_nome,
+                username=username,
+                email=email,
+                password=senha
+        )
+            messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso!')
+            return redirect('usuarios/cadastro/')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno, contato um administrador.')
+            return redirect('usuarios/cadastro/')
+        

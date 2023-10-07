815f2050efb65f9947acc9d1ba821523f788cd1d
vinicius-oliveiracr <vinicius.cr.o.3@gmail.com>
2023-10-07 18:56:21 -0300

    Implementação gerenciamento de exames

--- empresarial/views.py
+++ empresarial/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.db.models.functions import Concat
+from django.db.models import Value
+from django.contrib.admin.views.decorators import staff_member_required
+from exames.models import SolicitacaoExame
+from django.http import HttpResponse
 
 # Create your views here.
+
+@staff_member_required
+def gerenciar_clientes(request):
+    clientes = User.objects.filter(is_staff = False)
+
+    nome_completo = request.GET.get('nome')
+    email = request.GET.get('email')
+
+    if email:
+        clientes = clientes.filter(email__contains = email)
+
+    if nome_completo:
+        clientes = clientes.annotate(
+            full_name = Concat('first_name', Value(' '), 'last_name')
+        ).filter(full_name__contains=nome_completo)
+
+    return render (request, 'gerenciar_clientes.html', {'clientes': clientes})
+
+
+@staff_member_required 
+def cliente(request, cliente_id):
+    cliente = User.objects.get(id=cliente_id)
+    exames = SolicitacaoExame.objects.filter(usuario=cliente)
+    return render(request, 'cliente.html', {'cliente': cliente, 'exames': exames})
+
+@staff_member_required 
+def exame_cliente(request, exame_id):
+    exame = SolicitacaoExame.objects.get(id=exame_id)
+    return render (request, 'exame_cliente.html', {'exame':exame})
+
+
+def proxy_pdf(request, exame_id):
+    exame = SolicitacaoExame.objects.get(id=exame_id)
+
+    response = exame.resultado.open()
+
+    return HttpResponse(response)
